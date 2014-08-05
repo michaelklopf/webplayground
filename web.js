11540695e20c4354176627fbf8ddd79e91634a62
Michael Klopf <klopf.michael@gmail.com>
2014-08-05 18:27:53 +0200

    Added marionette example

--- web.js
+++ web.js
@@ -66,6 +66,11 @@ app.get('/polymer', function(req, res) {
   res.sendfile(__dirname + '/views/polymer.html');
 });
 
+app.get('/marionette', function(req, res) {
+  res.contentType('text/html');
+  res.render('marionette');
+});
+
 // For seeing changes without stoping, starting the server, install nodemon globally npm install -g nodemon. Start your server with nodemon server.js
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Listening on port ' + app.get('port'));
