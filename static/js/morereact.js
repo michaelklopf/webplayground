6ff33e78f9d746405cc995cf364645d19e18be97
Michael Klopf <klopf.michael@gmail.com>
2014-08-15 15:33:10 +0200

    Added comments, with MarkDown Support

--- static/js/morereact.js
+++ static/js/morereact.js
@@ -4,11 +4,56 @@
   <h1>Hello, world!</h1>,
   document.getElementById('example')
 );*/
+var converter = new Showdown.converter();
+
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+    );
+    // children is the child element at this place
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
 
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
         Hello, world! I am a CommentBox.
       </div>
     );
@@ -17,5 +62,5 @@ var CommentBox = React.createClass({
 
 React.renderComponent(
   <CommentBox />,
-  document.getElementById('content')
+  document.getElementById('example')
 );
