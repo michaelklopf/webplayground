22058b274cacbe11a95c4a6170e22bf9324140df
Michael Klopf <klopf.michael@gmail.com>
2014-09-12 23:33:49 +0200

    Added code to main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
 /**
   @jsx React.DOM
 */
+var React = require('react');
+
+var ApplicationComponent = React.createClass({
+  render:function(){
+    return (
+      <div>
+        <h1>Flux app headline.</h1>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(
+  <ApplicationComponent />,
+  document.getElementById('main')
+);
